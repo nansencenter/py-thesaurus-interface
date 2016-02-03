a5ff8e01ba0cba931510e70d8f7b1f03b4c77aa3
mortenwh <morten.wergeland.hansen@gmail.com>
2016-02-03 14:17:14 +0100

    added new tests - fails are by intention..

--- nerscmetadata/tests.py
+++ nerscmetadata/tests.py
@@ -37,5 +37,18 @@ class GetGCMDKeywordsTest(unittest.TestCase):
     def test_find_data_center(self):
         self.assertIsInstance(gcmd_keywords.get_data_center('NERSC'), dict)
 
-    def test_find_location(self):
+    def test_find_location_category(self):
+        gcmd_keywords.get_continent('continent')
+
+    def test_find_location_type(self):
+        gcmd_keywords.get_location_type('africa')
+
+    def test_find_location_subregion1(self):
+        gcmd_keywords.get_location_type('central africa')
+
+    def test_find_location_subregion2(self):
+        gcmd_keywords.get_location_type('Angola')
+
+    def test_find_location_subregion3(self):
         self.assertIsInstance(gcmd_keywords.get_location('HONG KONG'), dict)
+
