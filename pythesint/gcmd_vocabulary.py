c70c0a959548cc6489c4ebc57e2a2c2f9a4f038b
Aleksander Vines <aleksandervines1@hotmail.com>
2016-02-26 11:19:10 +0000

    moved read_line into own method

--- pythesint/gcmd_vocabulary.py
+++ pythesint/gcmd_vocabulary.py
@@ -23,30 +23,35 @@ class GCMDVocabulary(JSONVocabulary):
         do_record = False
 
         gcmd_list = []
+        kw_groups = []
         for line in response.readlines():
-            if 'Keyword Version' and 'Revision' in line:
-                meta = line.split('","')
-                gcmd_list.append({'Revision': meta[1][10:]})
-            if do_record:
-                gcmd_keywords = line.split('","')
-                gcmd_keywords[0] = gcmd_keywords[0].strip('"')
-                if gcmd_keywords[0] == 'NOT APPLICABLE':
-                    continue
-                # Remove last item (the ID is not needed)
-                gcmd_keywords.pop(-1)
-                if len(gcmd_keywords) != len(kw_groups):
-                    continue
-                line_kw = OrderedDict()
-                for i, key in enumerate(kw_groups):
-                    line_kw[key] = gcmd_keywords[i]
-                gcmd_list.append(line_kw)
-            if line.split(',')[0].lower() == self.categories[0].lower():
-                do_record = True
-                kw_groups = line.split(',')
-                kw_groups.pop(-1)
-                # Make sure the group items are as expected
-                if kw_groups != self.categories:
-                    raise TypeError('%s is not equal to %s' %
-                                    (kw_groups, self.categories))
+            read_line(line, gcmd_list, do_record, kw_groups)
 
         return gcmd_list
+
+
+def read_line(line, gcmd_list, do_record, kw_groups):
+    if 'Keyword Version' and 'Revision' in line:
+        meta = line.split('","')
+        gcmd_list.append({'Revision': meta[1][10:]})
+    if do_record:
+        gcmd_keywords = line.split('","')
+        gcmd_keywords[0] = gcmd_keywords[0].strip('"')
+        if gcmd_keywords[0] == 'NOT APPLICABLE':
+            continue
+        # Remove last item (the ID is not needed)
+        gcmd_keywords.pop(-1)
+        if len(gcmd_keywords) != len(kw_groups):
+            continue
+        line_kw = OrderedDict()
+        for i, key in enumerate(kw_groups):
+            line_kw[key] = gcmd_keywords[i]
+        gcmd_list.append(line_kw)
+    if line.split(',')[0].lower() == self.categories[0].lower():
+        do_record = True
+        kw_groups = line.split(',')
+        kw_groups.pop(-1)
+        # Make sure the group items are as expected
+        if kw_groups != self.categories:
+            raise TypeError('%s is not equal to %s' %
+                            (kw_groups, self.categories))
