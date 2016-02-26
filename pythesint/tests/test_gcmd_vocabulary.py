437acc405258261aee058dd0846556a1186d653c
Aleksander Vines <aleksandervines1@hotmail.com>
2016-02-26 13:30:17 +0000

    Added tests for _read_line

--- pythesint/tests/test_gcmd_vocabulary.py
+++ pythesint/tests/test_gcmd_vocabulary.py
@@ -36,6 +36,64 @@ class GCMDVocabularyTest(unittest.TestCase):
         with self.assertRaises(TypeError):
             pti.gcmd_vocabulary._check_categories(line, categories)
 
+    def test_read_line_simple(self):
+        gcmd_list = [{'Revision': '2016-01-08 13:40:40'}]
+        line = ('"Earth Remote Sensing Instruments","","","","","",'
+                '"6015ef7b-f3bd-49e1-9193-cc23db566b69"')
+        categories = ['Category', 'Class', 'Type', 'Subtype', 'Short_Name',
+                      'Long_Name']
+        pti.gcmd_vocabulary._read_line(line, gcmd_list, categories)
+        self.assertEqual(len(gcmd_list), 2)
+        self.assertEqual(gcmd_list[1], {'Category':
+                                        'Earth Remote Sensing Instruments',
+                                        'Class': '',
+                                        'Type': '',
+                                        'Subtype': '',
+                                        'Short_Name': '',
+                                        'Long_Name': ''})
+
+    def test_read_line_full(self):
+        gcmd_list = [{'Revision': '2016-01-08 13:40:40'}]
+        line = ('"Earth Remote Sensing Instruments","Active Remote Sensing",'
+                '"Altimeters","Lidar/Laser Altimeters","GLAS","Geoscience '
+                'Laser Altimeter System",'
+                '"57463f12-2a21-49f9-9477-718030d34291"')
+        categories = ['Category', 'Class', 'Type', 'Subtype', 'Short_Name',
+                      'Long_Name']
+        pti.gcmd_vocabulary._read_line(line, gcmd_list, categories)
+        self.assertEqual(len(gcmd_list), 2)
+        self.assertEqual(gcmd_list[1], {'Category':
+                                        'Earth Remote Sensing Instruments',
+                                        'Class': 'Active Remote Sensing',
+                                        'Type': 'Altimeters',
+                                        'Subtype': 'Lidar/Laser Altimeters',
+                                        'Short_Name': 'GLAS',
+                                        'Long_Name':
+                                        'Geoscience Laser Altimeter System'})
+
+    def test_read_line_not_applicable(self):
+        gcmd_list = [{'Revision': '2016-01-08 13:40:40'}]
+        line = ('"NOT APPLICABLE","","","","","",'
+                '"8129a4b9-b5f9-4585-87e6-4576c3a53682"')
+        categories = ['Category', 'Class', 'Type', 'Subtype', 'Short_Name',
+                      'Long_Name']
+        pti.gcmd_vocabulary._read_line(line, gcmd_list, categories)
+        self.assertEqual(len(gcmd_list), 1)
+        self.assertEqual(gcmd_list[0], {'Revision': '2016-01-08 13:40:40'})
+
+    def test_read_line_wrong_length(self):
+        gcmd_list = [{'Revision': '2016-01-08 13:40:40'}]
+        line = ('"Earth Remote Sensing Instruments","Active Remote Sensing",'
+                '"Altimeters","Lidar/Laser Altimeters","GLAS","Geoscience '
+                'Laser Altimeter System","Sneaky Extra Element",'
+                '"57463f12-2a21-49f9-9477-718030d34291"')
+        categories = ['Category', 'Class', 'Type', 'Subtype', 'Short_Name',
+                      'Long_Name']
+        pti.gcmd_vocabulary._read_line(line, gcmd_list, categories)
+        self.assertEqual(len(gcmd_list), 1)
+        self.assertEqual(gcmd_list[0], {'Revision': '2016-01-08 13:40:40'})
+
+
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
