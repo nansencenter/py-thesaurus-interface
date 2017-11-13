074d4c75592cc680f680d8d9928fbc3286ceed83
Aleksander Vines <aleksandervines1@hotmail.com>
2017-11-13 12:01:55 +0100

    Bumped version for next release

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ class update_vocabularies(install_scripts):
 setup(
     name=NAME,
 
-    version='1.2.0',
+    version='1.3.0',
 
     description='A Python interface to various metadata vocabularies',
     long_description=long_description,
@@ -46,7 +46,7 @@ setup(
 
     url='https://github.com/nansencenter/py-thesaurus-interface',
 
-    download_url='https://github.com/nansencenter/py-thesaurus-interface/archive/v1.2.0.tar.gz',
+    download_url='https://github.com/nansencenter/py-thesaurus-interface/archive/v1.3.0.tar.gz',
 
     packages=find_packages(),
 
