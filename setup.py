e05fc7c4deb0c4480b24f0f6518716c8cb68387e
Aleksander Vines <aleksandervines1@hotmail.com>
2017-12-15 09:34:42 +0100

    fixed classifiers, python 3.5 is not supported

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Atmospheric Science',
         'Topic :: Scientific/Engineering :: Information Analysis',
