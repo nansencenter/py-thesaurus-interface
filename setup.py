d46f015d05d89d4c3be63ff908bac435033d7662
Adrien Perrin <adrien.perrin@nersc.no>
2020-02-13 16:37:43 +0100

    update version number in setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ class update_vocabularies(install_scripts):
 setup(
     name=NAME,
 
-    version='1.4.7',
+    version='1.4.8',
 
     description='A Python interface to various metadata vocabularies',
     long_description=long_description,
@@ -46,7 +46,7 @@ setup(
 
     url='https://github.com/nansencenter/py-thesaurus-interface',
 
-    download_url='https://github.com/nansencenter/py-thesaurus-interface/archive/v1.4.5.tar.gz',
+    download_url='https://github.com/nansencenter/py-thesaurus-interface/archive/v1.4.8.tar.gz',
 
     packages=find_packages(),
 
