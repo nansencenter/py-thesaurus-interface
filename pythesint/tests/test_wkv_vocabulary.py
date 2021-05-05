55f1e7ea26c45b78f0bf2cd8773c0d7b1418aa7f
Adrien Perrin <adrien.perrin@nersc.no>
2021-05-05 13:00:31 +0000

    add test for wkv fetch with version

--- pythesint/tests/test_wkv_vocabulary.py
+++ pythesint/tests/test_wkv_vocabulary.py
@@ -1,5 +1,6 @@
 import unittest
 
+import mock.mock as mock
 import requests
 
 from pythesint.wkv_vocabulary import WKVVocabulary
@@ -10,3 +11,35 @@ class WKVVocabularyTest(unittest.TestCase):
         voc = WKVVocabulary(name='test_voc', url='https://sdfghdfghd.nersc.no')
         with self.assertRaises(requests.RequestException):
             voc._fetch_online_data()
+
+    def test_fetch_version(self):
+        """Test that the specified version is fetched"""
+        voc = WKVVocabulary(
+            name='test_voc',
+            url='https://raw.githubusercontent.com/nansencenter/nersc-vocabularies/master/nansat_wkv.yml')
+        # We use the exception as side effect to skip the part of the
+        # method we are not testing.
+        # TODO: split _fetch_online_data() into several methods instead
+        with mock.patch('requests.get') as mock_get:
+            mock_get.return_value.text = '[]'
+            self.assertListEqual(voc._fetch_online_data(), [])
+            mock_get.assert_called_with(
+                'https://raw.githubusercontent.com/nansencenter/nersc-vocabularies/master/'
+                'nansat_wkv.yml')
+
+            self.assertListEqual(voc._fetch_online_data(version=None), [])
+            mock_get.assert_called_with(
+                'https://raw.githubusercontent.com/nansencenter/nersc-vocabularies/master/'
+                'nansat_wkv.yml')
+
+            self.assertListEqual(voc._fetch_online_data(version=''), [])
+            mock_get.assert_called_with(
+                'https://raw.githubusercontent.com/nansencenter/nersc-vocabularies/master/'
+                'nansat_wkv.yml')
+
+            self.assertListEqual(
+                voc._fetch_online_data(version='91912bb'),
+                [{'version': '91912bb'}])
+            mock_get.assert_called_with(
+                'https://raw.githubusercontent.com/nansencenter/nersc-vocabularies/91912bb/'
+                'nansat_wkv.yml')
