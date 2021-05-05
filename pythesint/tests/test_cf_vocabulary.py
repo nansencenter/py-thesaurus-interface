fb951b9e86e72cc9e4ea072b5152242401f3a4b0
Adrien Perrin <adrien.perrin@nersc.no>
2021-05-05 12:43:59 +0000

    add test for cf fetch with version

--- pythesint/tests/test_cf_vocabulary.py
+++ pythesint/tests/test_cf_vocabulary.py
@@ -1,4 +1,6 @@
 import unittest
+
+import mock.mock as mock
 import requests
 
 from pythesint.cf_vocabulary import CFVocabulary
@@ -8,3 +10,27 @@ class CFVocabularyTest(unittest.TestCase):
         voc = CFVocabulary(name='test_voc', url='https://sdfghdfghd.nersc.no')
         with self.assertRaises(requests.RequestException):
             voc._fetch_online_data()
+
+    def test_fetch_version(self):
+        """Test that the specified version is fetched"""
+        voc = CFVocabulary(name='test_voc', url='https://sdfghdfghd.nersc.no')
+        # We use the exception as side effect to skip the part of the
+        # method we are not testing.
+        # TODO: split _fetch_online_data() into several methods instead
+        with mock.patch('requests.get', side_effect=requests.RequestException) as mock_get:
+            with self.assertRaises(requests.RequestException):
+                voc._fetch_online_data()
+            mock_get.assert_called_with('https://sdfghdfghd.nersc.no', params={})
+
+            with self.assertRaises(requests.RequestException):
+                voc._fetch_online_data(version=None)
+            mock_get.assert_called_with('https://sdfghdfghd.nersc.no', params={})
+
+            with self.assertRaises(requests.RequestException):
+                voc._fetch_online_data(version='')
+            mock_get.assert_called_with('https://sdfghdfghd.nersc.no', params={})
+
+            with self.assertRaises(requests.RequestException):
+                voc._fetch_online_data(version='9.1.5')
+            mock_get.assert_called_with(
+                'https://sdfghdfghd.nersc.no', params={'version': '9.1.5'})
