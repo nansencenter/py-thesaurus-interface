20eab6e9f6eb8bfd76cda4a9a7a37263d12eeba5
Adrien Perrin <adrien.perrin@nersc.no>
2025-08-12 13:16:45 +0000

    upgrade GCMDVocabulary CSV parsing

--- pythesint/gcmd_vocabulary.py
+++ pythesint/gcmd_vocabulary.py
@@ -1,8 +1,9 @@
 from __future__ import absolute_import
 
-from collections import OrderedDict
+import csv
 import requests
 import warnings
+from collections import OrderedDict
 
 from pythesint.json_vocabulary import JSONVocabulary
 
@@ -31,57 +32,34 @@ class GCMDVocabulary(JSONVocabulary):
         except requests.RequestException:
             print("Could not get the vocabulary file at '{}'".format(self.url))
             raise
-        rlines = [line for line in r.text.splitlines()]
-        gcmd_list = []
 
-        _read_revision(rlines[0], gcmd_list)
+        lines = r.text.splitlines()
+        keywords = []
+        # Add version+revision information
+        self._read_revision(lines[0], keywords)
+        # parse actual CSV contents
+        reader = csv.DictReader(lines[1:], dialect='unix', restval='')
+        self._check_categories(reader.fieldnames)
+        keywords.extend(list(reader))
+        # remove UUID and extra fields
+        for kw in keywords[1:]:
+            for key in ('UUID', None):
+                try:
+                    del kw[key]
+                except KeyError:
+                    pass
 
-        categories = _get_categories(rlines)
-        self._check_categories(categories)
+        return keywords
 
-        for line in rlines[2:]:
-            _read_line(line, gcmd_list, categories)
-
-        return gcmd_list
-
-
-def _read_revision(line, gcmd_list):
-    ''' Reads the line, extracts the Revision into a new dictionary and appends
-    it to gcmd_list
-    '''
-    # TODO: Cast exception if not found?
-    if 'Keyword Version' and 'Revision' in line:
-        meta = line.split('","')
-        gcmd_list.append({
-            'Revision': meta[1][10:],
-            'Keyword Version': meta[0].split(': ')[1]
-        })
-
-
-def _get_categories(lines):
-    '''Get the categories from the lines read from the source'''
-    return lines[1].split(',')[:-1]
-
-
-def _read_line(line, gcmd_list, categories):
-    ''' Converts line into dictionary values for elements in the categories
-    appends the dictionary to gcmd_list
-    '''
-    gcmd_keywords = line.split('","')
-    gcmd_keywords[0] = gcmd_keywords[0].strip('"')
-    if gcmd_keywords[0] == 'NOT APPLICABLE':
-        return
-    # Remove last item (the ID is not needed)
-    gcmd_keywords.pop(-1)
-    # skip record if it is longer than the definition of categories
-    if len(gcmd_keywords) > len(categories):
-        return
-    line_kw = OrderedDict()
-    for i, key in enumerate(categories):
-        if i < len(gcmd_keywords):
-            # if the record is equal to definition of categories
-            line_kw[key] = gcmd_keywords[i]
-        else:
-            # if the record is shorter than definition of categories: add empty string
-            line_kw[key] = ""
-    gcmd_list.append(line_kw)
+    @staticmethod
+    def _read_revision(line, gcmd_list):
+        ''' Reads the line, extracts the Revision into a new dictionary and appends
+        it to gcmd_list
+        '''
+        # TODO: Cast exception if not found?
+        if 'Keyword Version' and 'Revision' in line:
+            meta = line.split('","')
+            gcmd_list.append({
+                'Revision': meta[1][10:],
+                'Keyword Version': meta[0].split(': ')[1]
+            })
